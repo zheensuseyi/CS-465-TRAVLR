3e44715d6f5beb67f5f04d9f57837bc6ee30efe4
zheensuseyi <zheen.suseyi@snhu.edu>
2023-10-11 21:57:24 -0700

    Module 3 Update

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,9 @@
+var fs = require("fs");
+var trips = JSON.parse(fs.readFileSync("./data/trips.json", "utf8"));
+
 const travel = (req, res) => {
-    res.render("travel", { title: "Travlr Getaways" });
-  };
-  module.exports = {
-    travel,
-  };
+  res.render("travel", { title: "Travlr Getaways", trips });
+};
+module.exports = {
+  travel,
+};
